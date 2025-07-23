9c1aef51a5c0a4109231b49f5bcae818a8a0c7dd
krishnameghwal06 <krishnaa0604@gmail.com>
2025-07-23 22:50:51 +0530

    he

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,14 @@ import companyRoute from "./Routes/company.route.js";
 import jobRoute from "./Routes/job.route.js";
 import applicationRoute from "./Routes/application.route.js";
 import dotenv from "dotenv";
+import path from "path";
 dotenv.config({});
 
 const app = express(); // Moved this up
+
+
+
+const _dirname=path.resolve()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,6 +32,12 @@ app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job",jobRoute);
 app.use("/api/v1/application",applicationRoute);
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (_,res)=>{
+  res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+
+})
 
 app.listen(port, () => {
   connectDB();
